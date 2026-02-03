98c389e22e10bc1c0f882e81c0de88e962f7823f
Cesahz <espinolac999@gmail.com>
2026-02-03 13:47:42 -0300

    Version casi completa sin sonido/interfaz, correccion de errores y optimizacion de minijuegos

--- Escenas/Scripts/Puerta.gd
+++ Escenas/Scripts/Puerta.gd
@@ -1,13 +1,12 @@
 extends StaticBody2D
 
-# ¿Qué item necesito para abrir esto? (Debe coincidir con el nombre del Item)
+# ¿Qué item necesito para abrir esto?
 @export var llave_necesaria: String = "llave_pasillo"
 
 # Esta función la llamará tu Jugador al presionar el botón de interactuar
 func interactuar():
 	if Global.tiene_item(llave_necesaria):
 		print("¡Abriendo puerta!")
-		queue_free() # Borra la puerta (se abre)
-		# Opcional: Aquí podrías poner una animación o sonido antes de borrarla
+		queue_free() 
 	else:
 		print("Está cerrada. Necesitas: " + llave_necesaria)
